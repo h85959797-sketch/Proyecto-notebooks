066a4912a017204e41bbd1f5d6cc8e053f24da27
Luis <h85959797@gmail.com>
2026-09-17 15:17:22 -0300

    conexion bd

--- Backend/rutas/notebooks.js
+++ Backend/rutas/notebooks.js
@@ -6,10 +6,29 @@ const router = express.Router
 //Agregar una notebook
 router.post("/", async(req, res) => {
      const {
-      id_notebooks,
       marca,
       empresa,
       num_serie,
       estado
      } = req.body;
+
+     try {
+          const[resultado] = await pool.execute(
+               `INSERT INTO notebooks (marca, empresa, num_serie, estado) VALUES (?, ?, ?, ?)`,
+               [
+               marca,
+               empresa,
+               num_serie,
+               estado
+               ]
+          )
+          return res.status(201).json({
+               mensaje: "Producto agregado correctamente"
+          })
+     } catch (error) {
+          return res.status(500).json({
+               error: "No se pudo agregar el producto",
+               detalle: error.message
+          })
+     }
 });
--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,22 @@
 const express = require("express");
+const pool = require("./bd/conexion")
 
 const app=express();
 const PORT =3000;
 
 app.use(express.json());
 
+async function probarConexion(){
+    try {
+         await pool.query("SELECT 1");
+         console.log("Conexion exitosa")
+    } catch (error) {
+        console.log("Error en la conexion")
+        console.log(error.message)
+    }
+}
+
 app.listen(PORT, async () => {
     console.log(`Servidor ejecutable en http://localhost:${PORT}`)
+    await probarConexion()
 })
